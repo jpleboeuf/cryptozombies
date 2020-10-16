1cc0a1488f2bdc7939f470114f0522063acabb31
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-16 22:24:27 +0200

    Continue Lesson 1: "Making the Zombie Factory"
    With Chapter 8: "Working With Structs and Arrays".
    
    Answered by writing `Contract.sol`.

--- Lesson_01/chapter_08.sol
+++ Lesson_01/chapter_08.sol
@@ -13,7 +13,7 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function createZombie (string memory _name, uint _dna) public {
-        // start here
+        zombies.push(Zombie(_name, _dna));
     }
 
 }

fbd4e11eba415a71308928083179470499c948d2
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-16 15:21:24 +0200

    Continue Lesson 1: "Making the Zombie Factory"
    With Chapter 5: "Structs".
    
    Answered by writing `Contract.sol`.

--- Lesson_01/chapter_05.sol
+++ Lesson_01/chapter_05.sol
@@ -5,6 +5,9 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    // start here
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 
 }
